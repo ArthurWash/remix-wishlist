6623c6461944fa51e15e33e298fff06293b3ca51
AJ <ajw53@NJIT.edu>
2024-10-24 14:25:38 -0400

    Feature: create Form UI

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -11,13 +11,16 @@ import {
   Divider,
   TextField,
   useBreakpoints,
-  
+  Button,
+
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
+import { useState } from "react";
 
 export default function SettingsPage() {
 
-  const { smUp } = useBreakpoints();
+  const [formState, setFormState] = useState({});
+
   return (
     <Page>
       <TitleBar title="Settings" />
@@ -30,40 +33,25 @@ export default function SettingsPage() {
           >
             <BlockStack gap="400">
               <Text as="h3" variant="headingMd">
-                InterJambs
+                Settings
               </Text>
               <Text as="p" variant="bodyMd">
-                Interjambs are the rounded protruding bits of your puzzlie piece
+                Update app settings and preferences.
               </Text>
             </BlockStack>
           </Box>
           <Card roundedAbove="sm">
             <BlockStack gap="400">
-              <TextField label="Interjamb style" />
-              <TextField label="Interjamb ratio" />
-            </BlockStack>
-          </Card>
-        </InlineGrid>
-        {smUp ? <Divider /> : null}
-        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
-          <Box
-            as="section"
-            paddingInlineStart={{ xs: 400, sm: 0 }}
-            paddingInlineEnd={{ xs: 400, sm: 0 }}
-          >
-            <BlockStack gap="400">
-              <Text as="h3" variant="headingMd">
-                Dimensions
-              </Text>
-              <Text as="p" variant="bodyMd">
-                Interjambs are the rounded protruding bits of your puzzlie piece
-              </Text>
-            </BlockStack>
-          </Box>
-          <Card roundedAbove="sm">
-            <BlockStack gap="400">
-              <TextField label="Horizontal" />
-              <TextField label="Interjamb ratio" />
+              {/* The below code simply lets us type into the form. */}
+              <TextField label="App name" value={formState.name} onChange={(value) => setFormState({ ...formState, name: value })}/>
+                {/* value={formState.name} sets the value to name property. 
+                  onChange/setFormState defines an event handler for when the textfield's value changes. 
+                  The function takes the new value as an argument and updates the formState object using setFormState function. 
+                  The spread operator (...formState) ensure that the other properties in formState remain unchanged, while the name property is updated.
+                  */}
+              <TextField label="Description" value={formState.description} onChange={(value) => setFormState({ ...formState, description: value })} />
+
+              <Button submit={true}>Save</Button>
             </BlockStack>
           </Card>
         </InlineGrid>
